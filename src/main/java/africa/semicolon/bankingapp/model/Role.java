2839b27469838fb92fc2e52aaa43df073aac81ba
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-09-26 03:01:44 +0100

    updated

---
+++ src/main/java/africa/semicolon/bankingapp/model/Role.java
@@ -0,0 +1,38 @@
+package africa.semicolon.bankingapp.model;
+
+
+import africa.semicolon.bankingapp.model.enums.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Entity
+public class Role {
+    @Id
+    @Column
+    @SequenceGenerator(name= "role_id_sequence",
+    sequenceName = "role_id_sequence")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "role_id_sequence"
+    )
+    private Long id;
+
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    public Role(RoleType roleType) { this.roleType = roleType;
+
+    }
+
+    public Role() {
+
+    }
+
+}
---
+++ src/main/java/africa/semicolon/bankingapp/model/enums/RoleType.java
@@ -0,0 +1,5 @@
+package africa.semicolon.bankingapp.model.enums;
+
+public enum RoleType {
+    ROLE_ADMIN, ROLE_USER
+}
