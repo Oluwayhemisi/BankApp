e8296be5dcebc5f7670291c92c172d0f0bfbb0f8
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-09-26 03:01:16 +0100

    updated

---
+++ src/main/java/africa/semicolon/bankingapp/dto/responses/GetAccountInfoResponse.java
@@ -0,0 +1,8 @@
+package africa.semicolon.bankingapp.dto.responses;
+
+import africa.semicolon.bankingapp.model.Account;
+
+public class GetAccountInfoResponse {
+    private Account account;
+    private String message;
+}
