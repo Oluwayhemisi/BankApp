b4b15b08e3f4ecf52e1cdf6612d180a60804a4ee
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-09-27 16:34:02 +0100

    updated

--- src/main/java/africa/semicolon/bankingapp/controller/AccountController.java
+++ src/main/java/africa/semicolon/bankingapp/controller/AccountController.java
@@ -4,6 +4,7 @@ package africa.semicolon.bankingapp.controller;
 import africa.semicolon.bankingapp.dto.requests.CreateAccountRequest;
 import africa.semicolon.bankingapp.dto.responses.AccountInfoResponse;
 import africa.semicolon.bankingapp.dto.responses.ApiResponse;
+import africa.semicolon.bankingapp.exceptions.AccountException;
 import africa.semicolon.bankingapp.services.AccountService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,30 +15,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-@RequestMapping("/api/v1/account/")
+@RequestMapping("/api/v1/auth/")
 @AllArgsConstructor
 @Slf4j
 public class AccountController {
     private final AccountService accountService;
 
-    @PostMapping("create/")
-    public ResponseEntity<?> createAccount(@RequestBody CreateAccountRequest createAccountRequest){
-        log.info("yes i'm in the controller");
-        try{
-            AccountInfoResponse accountInfoResponse = accountService.createAccount(createAccountRequest);
-            ApiResponse apiResponse = ApiResponse.builder()
-                    .status("success")
-                    .message("Account created successfully")
-                    .data(accountInfoResponse)
-                    .build();
-            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
-        }catch (Exception e){
-            ApiResponse apiResponse = ApiResponse.builder()
-                    .status("failed")
-                    .message(e.getMessage())
-                    .build();
-            return new ResponseEntity<>(apiResponse,HttpStatus.valueOf(400));
+    @PostMapping("signup/")
+    public ResponseEntity<?> createAccount(HttpServletRequest request, @RequestBody CreateAccountRequest createAccountRequest) throws AccountException {
+    String host = request.getRequestURL().toString();
+    int index = host.indexOf("/",host.indexOf("/", host.indexOf("/")) +2);
+    host = host.substring(0,index+1);
+    log.info("Host -> {}",host);
+    AccountInfoResponse accountInfoResponse = accountService.createAccount(createAccountRequest);
+        ApiResponse apiResponse = ApiResponse.builder()
+                .status("success")
+                .message("Account created successfully")
+                .data(accountInfoResponse)
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+
+
+
+
+
+
         }
     }
-}
+
