df35360f6c9b06a0efd463010f7e1a7119ee071d
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-10-03 14:48:17 +0100

    updated

--- src/main/java/africa/semicolon/bankingapp/repository/CustomerRepository.java
+++ src/main/java/africa/semicolon/bankingapp/repository/CustomerRepository.java
@@ -2,6 +2,10 @@ package africa.semicolon.bankingapp.repository;
 
 import africa.semicolon.bankingapp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 }
