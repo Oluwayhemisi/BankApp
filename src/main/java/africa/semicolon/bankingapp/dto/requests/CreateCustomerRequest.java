efe412ead0664dd8c0a6f31100c58c7f521eb80f
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-10-03 14:47:20 +0100

    updated

---
+++ src/main/java/africa/semicolon/bankingapp/dto/requests/CreateCustomerRequest.java
@@ -0,0 +1,19 @@
+package africa.semicolon.bankingapp.dto.requests;
+
+import africa.semicolon.bankingapp.model.Account;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@NoArgsConstructor
+@Builder
+public class CreateCustomerRequest {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private Account account;
+}
