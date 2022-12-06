066143fcc17a9c2a8b7b69e016ae223f9e98b3e5
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-12-06 23:05:00 +0100

    updated

--- src/main/java/africa/semicolon/bankingapp/appconfig/AppConfiguration.java
+++ src/main/java/africa/semicolon/bankingapp/appconfig/AppConfiguration.java
@@ -1,29 +1,16 @@
 package africa.semicolon.bankingapp.appconfig;
 
 
-import africa.semicolon.bankingapp.repository.AccountRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.ui.Model;
+
 
 @Configuration
 public class AppConfiguration {
-
-//    @Bean
-//    public AccountRepository accountRepository(){
-//        return new AccountRepositoryImpl();
-//    }
-
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
 
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        return new UserDetailsService;
-//    }
 }
